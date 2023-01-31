77ab85323abfd095346b4f1ecf3f1abc4fe1b86e
Ray Alcides <railayli2@hotmail.com>
2023-01-31 20:07:57 -0300

    iniciando o desefio full stack

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+export function Login() {
+  return(
+  <div>
+     <i></i>
+    <div>
+      <input type="Email" />
+      <input type="Senha" />
+    </div>
+    <button>Login</button>
+  </div>
+  )
+ 
+}
